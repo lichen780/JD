99df939684de41eeaf12a999a54cf4ffe763f667
432234234 <lichen@tmpbox.net>
2021-11-03 08:48:14 +0800

    更新错误

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -25,6 +25,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let appId = '1EFRXxg' , homeDataFunPrefix = 'interact_template', collectScoreFunPrefix = 'harmony', message = ''
 let lotteryResultFunPrefix = homeDataFunPrefix, browseTime = 6
 const inviteCodes = [
+'',
 ];
 const randomCount = $.isNode() ? 20 : 5;
 const notify = $.isNode() ? require('./sendNotify') : '';
