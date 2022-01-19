36fce25ffa77606e1b43d37010913451952a3889
432234234 <1830934681@qq.com>
2022-01-19 21:36:52 +0800

    店铺签到 极速版签到 摇摇乐

--- jd_dpqd.js
+++ jd_dpqd.js
@@ -3,14 +3,14 @@
 ============Quantumultx===============
 [task_local]
 #店铺签到
-15 15 * * ** * * https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js, tag=店铺签到, enabled=true
+15 15 * * * https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js, tag=店铺签到, enabled=true
 ===========Loon============
 [Script]
-cron "15 15 * * ** * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js,tag=店铺签到
+cron "15 15 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js,tag=店铺签到
 ============Surge=============
-店铺签到 = type=cron,cronexp="15 15 * * ** * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js
+店铺签到 = type=cron,cronexp="15 15 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js
 ===========小火箭========
-店铺签到 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.jss, cronexpr="15 15 * * ** * *", timeout=3600, enable=true
+店铺签到 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.jss, cronexpr="15 15 * * *", timeout=3600, enable=true
 */
 const $ = new Env('店铺签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -26,24 +26,20 @@ let num=0
 let shopname=''
 const token = [
   "C22CEFD33E8331E6859BEC93C2BDF537",
-  "F932E5DE653968BB1D92A2DFFD5318C7",
+  "851A07C25A65DF722EF2731EBFF7CBE1",
   "C4FF5A919CEE68F9669A32950697694C",
-  "CF1BC3B231F11B3D89B91796944A7BA5",
   "6AC2C359FD6F8676976E8D355F777E1D",
-  "5839BF44D9EE68B28A2B9EF7E55E7DC9",
   "7A64973CD1A0873108CB2E4740A86D7D",
-  "621A2C5B99EC5DDAD2C30EFACC5CE2E0",
-  "45DBA1666FC56F3040F2F68396F1CDCE",
-  "91B152F4FE6110D612C16DFD5035B3FB",
+  "C7048BA40D33EEFFBFABA3714A7A4693",
+  "03359C20CCADB9E97008C940377DE6D1",
+  "A1380FC31845A30205790D13BF3C61F0",
   "098FD50396245FA00EA629B20A533E3E",
-  "651F433A62C413D64D446945E64ED013",
+  "AFC71B4BFEFD168AF0889313EC642ABB",
   "F12032C7804D496FC82CCCBEDE205CA0",
   "755A1AA058A02D18CCB4D239E35FCB5B",
-  "4CA02475CEFE1F2C7D704C47636A0F1E",
-  "91B152F4FE6110D612C16DFD5035B3FB",
+  "5AED62EAD871BE2882DA8F3BFA75058C",
   "88C72E9A4F342606C7C98C601CCB3196",
-  "020702C2C96800D57EAB2FA017314A42",
-  "329C618CD7B700B350C8CC8054B8622E",
+  "9F381F12C3F055A10876E2F70A0CB879",
   "7BFD202E8D2D071F5E79B6E9D3892020"
 ]
 
