69050c84bfc52cca2100659cc1705c1150b46ce2
432234234 <1830934681@qq.com>
2022-01-22 20:16:32 +0800

    摇摇乐 牧场 店铺签到

--- jd_ttysq.js
+++ jd_ttysq.js
@@ -52,9 +52,9 @@ const JD_API_HOST = `https://m.jingxi.com`;
             }
         }
     }
-    let res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-Sc22ripts/raw/master/shareCodes/ttysq2.json')
+    let res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-S2332cripts/raw/master/shareCodes/ttysq2.json')
     if (!res) {
-        res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-Sc22ripts/raw/master/shareCodes/ttysq2.json')
+        res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-S3232cripts/raw/master/shareCodes/ttysq2.json')
     }
     if (res) {
         authorCode = res.sort(() => 0.5 - Math.random())
@@ -97,7 +97,7 @@ const JD_API_HOST = `https://m.jingxi.com`;
                         console.log(`助力任务`)
                         await task(`jxnhj/DoTask`, `taskId=${$.taskId}&strShareId=${$.shareCoseList[y].code}&bizCode=jxnhj_task&configExtra=`);
                         if ($.max === true){$.shareCoseList[y].beHelp = false}
-                        await $.wait(4000);
+                        await $.wait(6000);
                         if ($.canHelp === false) { break }
                     }
                 }
@@ -133,7 +133,7 @@ const JD_API_HOST = `https://m.jingxi.com`;
                         console.log(`助力红包，Id: ${$.shareCoseList[y].redId}`)
                         await task(`jxnhj/BestWishes`, `shareId=${$.shareCoseList[y].code}&id=${$.shareCoseList[y].redId}`);
                         if ($.goHelp === true) {
-                            await $.wait(2000)
+                            await $.wait(3000)
                             await task(`jxnhj/WishHelp`, `id=${$.shareCoseList[y].redId}&shareId=${$.shareCoseList[y].code}`);
                             $.doHelpTimes += 1;
                             $.shareCoseList[y].beHelp += 1;
@@ -199,7 +199,7 @@ async function main() {
                     console.log(`去做${$.taskName}`)
                     await task(`jxnhj/DoTask`, `taskId=${$.taskId}&strShareId=&bizCode=jxnhj_task&configExtra=`)
                     console.log(`等待6秒`)
-                    await $.wait(6100)
+                    await $.wait(8100)
                     await task(`newtasksys/newtasksys_front/Award`, `taskId=${$.taskId}&bizCode=jxnhj_task&source=jxnhj_task`)
                 }
                 if ([4].includes($.oneTask.taskType)) {
